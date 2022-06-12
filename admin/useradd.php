1b1c2d68069dcbe127e11e053a1681a452bc3f26
Muhammad Ridwan Eryansah <mridwane01@gmail.com>
2022-06-12 16:21:14 +0700

    benerin user akses dan tampilannya

--- admin/useradd.php
+++ admin/useradd.php
@@ -8,16 +8,20 @@ include 'theme/header.php';
 include 'theme/sidebar.php';
 ?>
 <style type="text/css">
-  .error-msg{
-  text-align: center;
-  font-weight: bold;
-}
+  .error-msg {
+    text-align: center;
+    font-weight: bold;
+  }
 </style>
 <div class="container">
   <div class="card card-register mx-auto mt-3">
-        <div class="card-header"><center><h3>Tambahkan Pengguna</h3></center></div>
-        <div class="card-body">
-          <?php 
+    <div class="card-header">
+      <center>
+        <h3>Tambahkan Pengguna</h3>
+      </center>
+    </div>
+    <div class="card-body">
+      <?php 
           if (isset($_GET['error'])) {
             if ($_GET["error"]=="emptyfields") {
               echo '<p class="text-danger error-msg">Isi semua kolom</p>';
@@ -41,76 +45,78 @@ include 'theme/sidebar.php';
              
          
            ?>
- <form action="theme/signup.php" method="post">
-            <div class="form-group">
-              <div class="form-row">
-                <div class="col-md-6">
-                  <div class="form-label-group">
-                    <input type="text" id="firstName" name="firstname" class="form-control" placeholder="First name" autofocus="autofocus">
-                    <label for="firstName">Nama Depan</label>
-                  </div>
-                </div>
-                <div class="col-md-6">
-                  <div class="form-label-group">
-                    <input type="text" id="lastName" name="lastname" class="form-control" placeholder="Last name">
-                    <label for="lastName">Nama Belakang</label>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="form-group">
+      <form action="theme/signup.php" method="post">
+        <div class="form-group">
+          <div class="form-row">
+            <div class="col-md-6">
               <div class="form-label-group">
-                <input type="text" id="inputEmail" name="mail" class="form-control" placeholder="Email address">
-                <label for="inputEmail">Email</label>
+                <input type="text" id="firstName" name="firstname" class="form-control" placeholder="First name"
+                  autofocus="autofocus">
+                <label for="firstName">Nama Depan</label>
               </div>
             </div>
-            <div class="form-group">
+            <div class="col-md-6">
               <div class="form-label-group">
-                <select type="text" id="inputEmail2" name="pos" class="form-control" placeholder="Position">
-                  <option>Admin</option>
-                  <option>Kurir</option>
-                  <option>Pemilik</option>
-                </select>
+                <input type="text" id="lastName" name="lastname" class="form-control" placeholder="Last name">
+                <label for="lastName">Nama Belakang</label>
               </div>
             </div>
-            <div class="form-group">
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="form-label-group">
+            <input type="text" id="inputEmail" name="mail" class="form-control" placeholder="Email address">
+            <label for="inputEmail">Email</label>
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="form-label-group">
+            <select type="text" id="inputEmail2" name="pos" class="form-control" placeholder="Position">
+              <option>Crew Restoran</option>
+              <option>Admin Finance</option>
+              <option>Operational Manajer</option>
+            </select>
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="form-label-group">
+            <input type="hidden" id="inputContact" name="con" class="form-control" placeholder="Email address">
+            <label for="inputContact">Contact</label>
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="form-label-group">
+            <input type="hidden" id="inputAddress" name="addre" class="form-control" placeholder="Email address">
+            <label for="inputAddress">Alamat</label>
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="form-label-group">
+            <input type="text" id="inputEmail1" name="uid" class="form-control" placeholder="User Name">
+            <label for="inputEmail1">Nama Pengguna</label>
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="form-row">
+            <div class="col-md-6">
               <div class="form-label-group">
-                <input type="hidden" id="inputContact" name="con" class="form-control" placeholder="Email address">
-                <label for="inputContact">Contact</label>
+                <input type="password" id="inputPassword" name="pwd" class="form-control" placeholder="Password">
+                <label for="inputPassword">Kata Sandi</label>
               </div>
             </div>
-            <div class="form-group">
+            <div class="col-md-6">
               <div class="form-label-group">
-                <input type="hidden" id="inputAddress" name="addre" class="form-control" placeholder="Email address">
-                <label for="inputAddress">Alamat</label>
+                <input type="password" id="confirmPassword" name="pwdcon" class="form-control"
+                  placeholder="Confirm password">
+                <label for="confirmPassword">Konfirmasi Kata Sandi</label>
               </div>
             </div>
-            <div class="form-group">
-              <div class="form-label-group">
-                <input type="text" id="inputEmail1" name="uid" class="form-control" placeholder="User Name">
-                <label for="inputEmail1">Nama Pengguna</label>
-              </div>
-            </div>
-            <div class="form-group">
-              <div class="form-row">
-                <div class="col-md-6">
-                  <div class="form-label-group">
-                    <input type="password" id="inputPassword" name="pwd" class="form-control" placeholder="Password">
-                    <label for="inputPassword">Kata Sandi</label>
-                  </div>
-                </div>
-                <div class="col-md-6">
-                  <div class="form-label-group">
-                    <input type="password" id="confirmPassword" name="pwdcon" class="form-control" placeholder="Confirm password">
-                    <label for="confirmPassword">Konfirmasi Kata Sandi</label>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <button type="submit" class="btn btn-primary btn-block" name="signup-submit">Simpan</button>
-          </form>  
-                       </div>
-                </div>
-                </div>
+          </div>
+        </div>
+        <button type="submit" class="btn btn-primary btn-block" name="signup-submit">Simpan</button>
+      </form>
+    </div>
+  </div>
+</div>
 <!--footer area-->
 <?php include 'theme/footer.php'; }?>
