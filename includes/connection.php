fd9230967842fe197e8b05abb610c6350e5647d4
Muhammad Ridwan Eryansah <mridwane01@gmail.com>
2022-06-12 16:22:29 +0700

    benerin nama database

--- includes/connection.php
+++ includes/connection.php
@@ -1,5 +1,5 @@
 <?php
  $db = mysqli_connect('localhost', 'root', '') or
         die ('Unable to connect. Check your connection parameters.');
-        mysqli_select_db($db, 'pbh_db' ) or die(mysqli_error($db));
+        mysqli_select_db($db, 'andakar' ) or die(mysqli_error($db));
 ?>
