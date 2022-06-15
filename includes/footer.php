1f3eaa706c72001a119567723775b1137a797631
Muhammad Ridwan Eryansah <mridwane01@gmail.com>
2022-06-15 18:43:01 +0700

    nambahin cart js

--- includes/footer.php
+++ includes/footer.php
@@ -102,6 +102,8 @@
  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCh39n5U-4IoWpsVGUHWdqB6puEkhRLdmI&callback=myMap">
  </script>
  <!-- End Google Map -->
+ <!-- cart js -->
+ <script src="assets/js/cart.js"></script>
 
  </body>
 
