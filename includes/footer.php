cc85d6b55ba5226c81fa149b9092d02e94a00b3c
Muhammad Ridwan Eryansah <mridwane01@gmail.com>
2022-06-25 00:48:12 +0700

    nambahin cdn sweet alert

--- includes/footer.php
+++ includes/footer.php
@@ -88,6 +88,8 @@
  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
      integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
  </script>
+ <!-- SweetAlert2 -->
+ <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
  <!-- bootstrap js -->
  <script src="assets/js/bootstrap.js"></script>
  <!-- owl slider -->
