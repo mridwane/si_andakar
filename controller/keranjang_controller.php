811e6070bfe18af0f9ffe2d811e910f89ce5352d
Muhammad Ridwan Eryansah <mridwane01@gmail.com>
2022-06-24 19:51:41 +0700

    keranjang untuk insert di aktifin

--- controller/keranjang_controller.php
+++ controller/keranjang_controller.php
@@ -28,64 +28,62 @@ if ($fungsi == 'Delete'){
     //  }
     //  else {
     //     echo 'Berhasild dihapus';
+} else {
+    // echo $kd_saus.$qty;
+    $query1 = 'SELECT * From tblcart WHERE c_id = "'.$cid.'"';
+    $result1 = mysqli_query($db, $query1) or die(mysqli_error($db));
+    $row = mysqli_fetch_assoc($result1);
+    if(!$row) {
+        $query1 = "INSERT INTO tblcart (kd_cart, jenis_cart, date, c_id) VALUES ('".$kd_cart."', '".$jenis_cart."',
+        '".$date."', '".$cid."')";
+        $result1 = mysqli_query($db, $query1) or die(mysqli_error($db));
+
+        $query2 = "INSERT INTO tblcartdetail (kd_cart, kd_menu, kd_saus, qty, harga) VALUES ('".$kd_cart."', '".$kd_menu."',
+        '".$kd_saus."', '".$qty."', '".$harga."')";
+        $result2 = mysqli_query($db, $query2) or die(mysqli_error($db));
+
+        // echo "<script>
+        //     alert('Berhasil menambahkan ke pesanan');
+        // </script>";
+        // if($result1){
+            // echo "<script>
+            //     alert('Berhasil menambahkan ke pesanan');
+            // </script>";
+        // }
+        // else {
+        //     echo "<script>
+        //         alert('Gagal menambahkan ke pesanan');
+        //     </script>";
+        // }
+
+        
+    }
+    else {
+
+        // $query = 'SELECT * From tblcartdetail WHERE c_id = "'.$cid.'" AND ';
+        // $result = mysqli_query($db, $query) or die(mysqli_error($db));
+        // $row = mysqli_fetch_assoc($result);
+
+        // if(!$row) {
+            $query2 = "INSERT INTO tblcartdetail (kd_cart, kd_menu, kd_saus, qty, harga) VALUES ('".$kd_cart."',
+            '".$kd_menu."',
+            '".$kd_saus."', '".$qty."', '".$harga."')";
+            $result2 = mysqli_query($db, $query2) or die(mysqli_error($db));
+        // }
+        
+        
+        // if($result2){
+        //     echo "<script>
+        //         alert('Berhasil menambahkan ke pesanan');
+        //     </script>";
+        // }
+        // else {
+        //     echo "<script>
+        //         alert('Gagal menambahkan ke pesanan');
+        //     </script>";
+        // }
+    }
 }
-
-// } else {
-//     // echo $kd_saus.$qty;
-//     $query1 = 'SELECT * From tblcart WHERE c_id = "'.$cid.'"';
-//     $result1 = mysqli_query($db, $query1) or die(mysqli_error($db));
-//     $row = mysqli_fetch_assoc($result1);
-//     if(!$row) {
-//         $query1 = "INSERT INTO tblcart (kd_cart, jenis_cart, date, c_id) VALUES ('".$kd_cart."', '".$jenis_cart."',
-//         '".$date."', '".$cid."')";
-//         $result1 = mysqli_query($db, $query1) or die(mysqli_error($db));
-
-//         $query2 = "INSERT INTO tblcartdetail (kd_cart, kd_menu, kd_saus, qty, harga) VALUES ('".$kd_cart."', '".$kd_menu."',
-//         '".$kd_saus."', '".$qty."', '".$harga."')";
-//         $result2 = mysqli_query($db, $query2) or die(mysqli_error($db));
-
-//         // echo "<script>
-//         //     alert('Berhasil menambahkan ke pesanan');
-//         // </script>";
-//         // if($result1){
-//             // echo "<script>
-//             //     alert('Berhasil menambahkan ke pesanan');
-//             // </script>";
-//         // }
-//         // else {
-//         //     echo "<script>
-//         //         alert('Gagal menambahkan ke pesanan');
-//         //     </script>";
-//         // }
-
-        
-//     }
-//     else {
-
-//         $query = 'SELECT * From tblcartdetail WHERE c_id = "'.$cid.'" AND ';
-//         $result = mysqli_query($db, $query) or die(mysqli_error($db));
-//         $row = mysqli_fetch_assoc($result);
-
-//         if(!$row) {
-//             $query2 = "INSERT INTO tblcartdetail (kd_cart, kd_menu, kd_saus, qty, harga) VALUES ('".$kd_cart."',
-//             '".$kd_menu."',
-//             '".$kd_saus."', '".$qty."', '".$harga."')";
-//             $result2 = mysqli_query($db, $query2) or die(mysqli_error($db));
-//         }
-        
-        
-//         // if($result2){
-//         //     echo "<script>
-//         //         alert('Berhasil menambahkan ke pesanan');
-//         //     </script>";
-//         // }
-//         // else {
-//         //     echo "<script>
-//         //         alert('Gagal menambahkan ke pesanan');
-//         //     </script>";
-//         // }
-//     }
-// }
 
 
 
