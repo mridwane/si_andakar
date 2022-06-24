6f085374443cc7029a15f1213a978ba559a28ae5
Muhammad Ridwan Eryansah <mridwane01@gmail.com>
2022-06-25 01:33:47 +0700

    benerin title di admin

--- admin/theme/header.php
+++ admin/theme/header.php
@@ -9,7 +9,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Purna Baja Harsco - Admin</title>
+  <title> Andakar - ADMIN</title>
 
   <!-- Bootstrap core CSS-->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
