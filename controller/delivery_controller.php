e3d37f0a3a04cf536fb79af0736bba8495d49304
Sandria Maulana <sandriamaulana31@gmail.com>
2022-06-26 18:54:39 +0700

    Upload bukti transfer. (alert saat berhasil upload belum memakai sweet alert)

--- controller/delivery_controller.php
+++ controller/delivery_controller.php
@@ -1,6 +1,7 @@
 <?php
 include('../includes/connection.php');
 
+
 session_start();
 if ($_GET['action'] == 'save') {
   $random = rand(10, 100);
@@ -13,7 +14,7 @@ if ($_GET['action'] == 'save') {
   // $jml_total = 120000;
   // $total_barang = $_POST['total_barang'];
 
-  
+
 
   // jika berhasil eksekusi
   // $data = array([
@@ -26,29 +27,89 @@ if ($_GET['action'] == 'save') {
   JOIN tblproducts c
   ON
   a.kd_menu = c.product_id JOIN tblsaus d ON
-  a.kd_saus = d.id_saus WHERE b.c_id = "'.$user_id.'"');
+  a.kd_saus = d.id_saus WHERE b.c_id = "' . $user_id . '"');
   $ht = mysqli_fetch_array($hitung_total);
   $jml_total = $ht['total'];
   $instransac = "INSERT INTO tbltransac (transac_code, date, transac_type, status, total_price, customer_id)
       VALUES ('$no_transac','$date','reservasi','4','$jml_total','$user_id')";
   mysqli_query($db, $instransac) or die('Error, gagal menyimpan data delivery');
-  
+
   // untuk memasukan ke table tbltransacdetail
   $query_select = mysqli_query($db, 'SELECT * FROM tblcartdetail a JOIN tblcart b ON a.kd_cart = b.kd_cart
   JOIN tblproducts c
   ON
   a.kd_menu = c.product_id JOIN tblsaus d ON
-  a.kd_saus = d.id_saus WHERE b.c_id = "'.$user_id.'"');
-  while($row = mysqli_fetch_array($query_select)){
+  a.kd_saus = d.id_saus WHERE b.c_id = "' . $user_id . '"');
+  while ($row = mysqli_fetch_array($query_select)) {
     $query2 = "INSERT INTO tbltransacdetail (product_code, kd_saus, qty, transac_code, harga)
-    VALUES ('" . $row['kd_menu']. "', '" . $row['kd_saus']. "', '" . $row['qty'] . "', '$no_transac' ,
-    '".$row['harga']."')";
+    VALUES ('" . $row['kd_menu'] . "', '" . $row['kd_saus'] . "', '" . $row['qty'] . "', '$no_transac' ,
+    '" . $row['harga'] . "')";
     mysqli_query($db, $query2) or die('Error, gagal menyimpan data delivery');
   }
   echo ("<script language='JavaScript'>
          window.location.href='../delivery.php?nt=$no_transac';
          window.alert('Data Delivery berhasil disimpan')
        </script>");
+}
+
+if ($_GET['action'] == 'savetrf') {
+
+  if (isset($_POST['confirm'])) {
+    if ($_FILES['upload']['name'] != "") {
+
+      $transac_code = $_POST['transac_code'];
+      $datetime = date('Y-m-d H:i:s');
+      // get file
+      $file = $_FILES['upload']['name'];
+      $tmp = $_FILES['upload']['tmp_name'];
+      $type = pathinfo($file, PATHINFO_EXTENSION);
+
+      $user_id = $_SESSION['cid'];
+
+
+      // Rename nama file
+      $filenew = "BT" . $transac_code;
+      // Set path folder tempat menyimpan fotonya
+      $path = "../assets/bukti_transfer/" . $filenew;
+
+
+
+      // Cek apakah gambar berhasil diupload atau tidak
+      $status = false;
+      if (move_uploaded_file($tmp, $path)) {
+        copy($path, $path);
+      }
+      if ($status == false) {
+
+        // Proses simpan ke Database
+
+        $nama = "";
+
+        if ($type != "") {
+          $nama = $filenew;
+        }
+
+        $query = "INSERT INTO `tblbuktitransfer`(`date`, `file_name`, `no_transac`)
+            VALUES ('" . $datetime . "','" . $nama . "','" . $transac_code . "')";
+        mysqli_query($db, $query) or die(mysqli_error($db));
+
+        // TODO: Gunakan sweet alert
+        echo ("<script language='JavaScript'>
+        window.location.href='../index.php';
+        window.alert('Terima kasih telah melakukan pemesanan! Pesanan anda akan kami proses dan dikirimkan mohon menunggu dan cek status pesanan di menu pesanan saya.')
+        </script>");
+      } else {
+        // Jika gambar gagal diupload, Lakukan :
+        echo "Maaf, butkti transfer gagal untuk diupload.";
+      }
+    } else {
+?>
+      <script type="text/javascript">
+        alert("Silahkan pilih foto bukti transfer terlebih dahulu untuk di upload");
+      </script>
+<?php
+    }
+  }
 }
 
 // elseif ($_GET['action'] == 'update') {
