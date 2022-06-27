9428afe31e8ba78ee91fb7657ed0226529864082
Muhammad Ridwan Eryansah <mridwane01@gmail.com>
2022-06-27 12:26:04 +0700

    ubah jquery untuk filter menu

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,25 +9,78 @@ getYear();
 
 
 // isotope js
-$(window).on('load', function () {
-    $('.filters_menu li').click(function () {
-        $('.filters_menu li').removeClass('active');
-        $(this).addClass('active');
-
-        var data = $(this).attr('data-filter');
-        $grid.isotope({
-            filter: data
-        })
-    });
-    var $grid = $(".grid").isotope({
-        itemSelector: ".all",
-        percentPosition: false,
-        masonry: {
-            columnWidth: ".all"
-        }
-    })
-
+// init Isotope
+var $grid = $('.grid').isotope({
+    itemSelector: '.all'
 });
+
+// store filter for each group
+var filters = {};
+
+$('.filters').on('click', '.button', function (event) {
+    var $button = $(event.currentTarget);
+    // get group key
+    var $buttonGroup = $button.parents('.filters_menu');
+    var filterGroup = $buttonGroup.attr('data-filter-group');
+    // set filter for group
+    filters[filterGroup] = $button.attr('data-filter');
+    // combine filters
+    var filterValue = concatValues(filters);
+    // set filter for Isotope
+    $grid.isotope({
+        filter: filterValue
+    });
+});
+
+// change is-checked class on buttons
+$('.filters_menu').each(function (i, filters_menu) {
+    var $filters_menu = $(filters_menu);
+    $filters_menu.on('click', '.button', function (event) {
+        $filters_menu.find('.active').removeClass('active');
+        var $button = $(event.currentTarget);
+        $button.addClass('active');
+    });
+});
+
+// flatten object by concatting values
+function concatValues(obj) {
+    var value = '';
+    for (var prop in obj) {
+        value += obj[prop];
+    }
+    return value;
+}
+// end isotopjs
+
+// $(window).on('load', function () {
+//     $('.filters_menu li').click(function () {
+//         $('.filters_menu li').removeClass('active');
+//         $(this).addClass('active');
+
+//         var data = $(this).attr('data-filter');
+//         $grid.isotope({
+//             filter: data
+//         })
+//     });
+
+//     $('.sub_filters li').click(function () {
+//         $('.sub_filters li').removeClass('active');
+//         $(this).addClass('active');
+
+//         var data = $(this).attr('data-filter');
+//         $grid.isotope({
+//             filter: data
+//         })
+//     });
+//     var $grid = $(".grid").isotope({
+//         itemSelector: ".all",
+//         percentPosition: false,
+//         masonry: {
+//             columnWidth: ".all"
+//         }
+//     })
+
+// });
 
 // nice select
 // $(document).ready(function () {
