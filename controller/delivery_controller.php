9e7a792ac1961ee8642ac5e42feb07948f7a1db6
Muhammad Ridwan Eryansah <mridwane01@gmail.com>
2022-06-28 14:05:11 +0700

    Benerin status sama jenis transaksi

--- controller/delivery_controller.php
+++ controller/delivery_controller.php
@@ -31,7 +31,7 @@ if ($_GET['action'] == 'save') {
   $ht = mysqli_fetch_array($hitung_total);
   $jml_total = $ht['total'];
   $instransac = "INSERT INTO tbltransac (transac_code, date, transac_type, status, total_price, customer_id)
-      VALUES ('$no_transac','$date','delivery','4','$jml_total','$user_id')";
+      VALUES ('$no_transac','$date','Delivery','pending','$jml_total','$user_id')";
   mysqli_query($db, $instransac) or die('Error, gagal menyimpan data delivery');
 
   // untuk memasukan ke table tbltransacdetail
@@ -110,9 +110,9 @@ if ($_GET['action'] == 'savetrf') {
       }
     } else {
 ?>
-      <script type="text/javascript">
-        alert("Silahkan pilih foto bukti transfer terlebih dahulu untuk di upload");
-      </script>
+<script type="text/javascript">
+  alert("Silahkan pilih foto bukti transfer terlebih dahulu untuk di upload");
+</script>
 <?php
     }
   }
