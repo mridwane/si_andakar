742da8515515e70af501716f3b9674a77c23bdd2
Muhammad Ridwan Eryansah <mridwane01@gmail.com>
2022-07-02 03:06:56 +0700

    fitur alamat selesai di detail transaksi admin

--- admin/detailtransac.php
+++ admin/detailtransac.php
@@ -8,13 +8,14 @@ if (!isset($_SESSION["userid"])) {
   include 'theme/sidebar.php';
 
   $query = 'SELECT *,concat(`C_FNAME`," ",`C_LNAME`)as name,`C_PNUMBER` FROM `tbltransac` a INNER JOIN
-  `tblcustomer` b on a.customer_id=b.C_ID WHERE a.transac_code ="' . $_GET['id'] . '"';
+  `tblcustomer` b on a.customer_id=b.C_ID INNER JOIN
+  `tblalamat` c on b.C_ADRESSID=c.id_alamat WHERE a.transac_code ="' . $_GET['id'] . '"';
   $result = mysqli_query($db, $query) or die(mysqli_error($db));
   while ($row = mysqli_fetch_array($result)) {
     $stats = $row['status'];
     $name = $row['name'];
     $contact = $row['C_PNUMBER'];
-    $address = $row['C_ADDRESS'];
+    $address = $row['alamat'];
     $tgl_reservasi = $row['reservation_date_time'];
     $cd = $row['transac_code'];
     $order_type = $row['transac_type'];
@@ -65,45 +66,49 @@ if (!isset($_SESSION["userid"])) {
 ?>
 
 
-  <div class="card">
-    <div class="card-header">
-      <div style="margin-bottom: 30px">
-        <h5>No. Pemesanan : <?php echo $cd; ?></h5>
-        <h5>Nama : <?php echo $name; ?></h5>
-        <h5>Kontak : 0<?php echo $contact; ?></h5>
-        <h5>Alamat : <?php echo $address; ?></h5>
-        <h5>Jenis Pesanan : <?php echo $order_type; ?></h5>
-        <h5>Status Pesanan : <?php echo $status_strng; ?></h5>
-        <h5>Tanggal & Waktu : <?php echo $tgl_reservasi; ?></h5>
-        <?php if (strtoupper($order_type) == strtoupper("delivery") || strtoupper($order_type) == strtoupper("catering")) { ?>
-          <?php if ($stats == "paid" || $stats == "dp" || $stats == "lunas" || $stats == "pelunasan" || $stats == "after_revision_lns" || $stats == "after_revision") { ?>
-            <h5>Bukti Transfer <?php if ($order_type == "Catering") {
+<div class="card">
+  <div class="card-header">
+    <div style="margin-bottom: 30px">
+      <h5>No. Pemesanan : <?php echo $cd; ?></h5>
+      <h5>Nama : <?php echo $name; ?></h5>
+      <h5>Kontak : 0<?php echo $contact; ?></h5>
+      <h5>Alamat : <?php echo $address; ?></h5>
+      <h5>Jenis Pesanan : <?php echo $order_type; ?></h5>
+      <h5>Status Pesanan : <?php echo $status_strng; ?></h5>
+      <h5>Tanggal & Waktu : <?php echo $tgl_reservasi; ?></h5>
+      <?php if (strtoupper($order_type) == strtoupper("delivery") || strtoupper($order_type) == strtoupper("catering")) { ?>
+      <?php if ($stats == "paid" || $stats == "dp" || $stats == "lunas" || $stats == "pelunasan" || $stats == "after_revision_lns" || $stats == "after_revision") { ?>
+      <h5>Bukti Transfer <?php if ($order_type == "Catering") {
                                   echo " DP";
-                                } ?>: <?php echo '<a href="controller/download_file_transaksi.php?file_name=' . $file_name_dp . '&no_transac=' . $cd . '">Download Bukti Transfer</a>'; ?></h5>
+                                } ?>:
+        <?php echo '<a href="controller/download_file_transaksi.php?file_name=' . $file_name_dp . '&no_transac=' . $cd . '">Download Bukti Transfer</a>'; ?>
+      </h5>
 
-          <?php }
+      <?php }
           if ($stats == "lunas" || $stats == "pelunasan" || $stats == "after_revision_lns") { ?>
-            <h5>Bukti Transfer Pelunasan : <?php echo '<a href="controller/download_file_transaksi.php?file_name=' . $file_name_lunas . '&no_transac=' . $cd . '">Download Bukti Transfer</a>'; ?></h5>
-        <?php
+      <h5>Bukti Transfer Pelunasan :
+        <?php echo '<a href="controller/download_file_transaksi.php?file_name=' . $file_name_lunas . '&no_transac=' . $cd . '">Download Bukti Transfer</a>'; ?>
+      </h5>
+      <?php
           }
         }  ?>
-      </div>
-      <div class="card-body">
-        <h4 style="color: blue">Informasi Pemesanan</h4>
-        <div class="table-responsive">
-          <table cellpadding="5" width="100%" cellspacing="0">
-            <thead>
-              <tr>
-                <th>Produk</th>
-                <th>Tanggal Pesanan</th>
-                <th>Jumlah</th>
-                <th>Harga Makanan</th>
-                <th>Harga Saus</th>
-                <th>Total</th>
-              </tr>
-            </thead>
-            <tbody style="font-size: 20px">
-              <?php
+    </div>
+    <div class="card-body">
+      <h4 style="color: blue">Informasi Pemesanan</h4>
+      <div class="table-responsive">
+        <table cellpadding="5" width="100%" cellspacing="0">
+          <thead>
+            <tr>
+              <th>Produk</th>
+              <th>Tanggal Pesanan</th>
+              <th>Jumlah</th>
+              <th>Harga Makanan</th>
+              <th>Harga Saus</th>
+              <th>Total</th>
+            </tr>
+          </thead>
+          <tbody style="font-size: 20px">
+            <?php
               // $query = 'SELECT * FROM `tbltransac` a, `tblproducts` b, `tbltransacdetail` c WHERE
               // c.product_code=b.product_id AND a.transac_code="'.$_GET['id'].'"';
               $query = 'SELECT * FROM `tbltransac` a
@@ -124,7 +129,7 @@ if (!isset($_SESSION["userid"])) {
                 echo '</tr> ';
               }
               ?>
-              <?php
+            <?php
               $query = 'SELECT * FROM tbltransac
                 WHERE
                 transac_code ="' . $_GET['id'] . '"';
@@ -133,7 +138,7 @@ if (!isset($_SESSION["userid"])) {
 
               ?>
 
-                <!-- <tr>
+            <!-- <tr>
               <td colspan="5" align="right"><br>
                 <h5> Subtotal :</h5>
               </td>
@@ -159,10 +164,10 @@ if (!isset($_SESSION["userid"])) {
             </tr> -->
 
 
-            </tbody>
-          </table>
-          <br>
-          <?php
+          </tbody>
+        </table>
+        <br>
+        <?php
                 $query = 'SELECT * FROM tbltransac
                 WHERE
                 transac_code ="' . $_GET['id'] . '"';
@@ -189,35 +194,43 @@ if (!isset($_SESSION["userid"])) {
 
           ?>
 
-          <!-- daftar tombol untuk type transaksi delivery -->
-          <?php if (strtoupper($row["status"]) == '0' && strtoupper($row["transac_type"]) == strtoupper("delivery")) { ?>
-            <a title="Cancel" type="button" class="btn btn-xs btn-danger " onclick="return confirm('Apakah Anda ingin membatalkan transaksi?')" href="confirm.php?action=edit & cancel=<?php echo $row['transac_code']; ?> "><i class="fas fa-minus-circle"></i>Batal</a>
-            <a title="Confirm" type="button" class="btn btn-xs btn-info " onclick="return confirm('Apakah Anda ingin mengkonfirmasi transaksi?')" href="confirm.php?action=edit & confirm=<?php echo $row['transac_code']; ?>"><i class="fas fa-check"></i>Konfirmasi</a>
-            <a href="detail.php" class="btn btn-xs btn-warning"><i class="fas fa-sign-out-alt"></i>Kembali</a>
+        <!-- daftar tombol untuk type transaksi delivery -->
+        <?php if (strtoupper($row["status"]) == '0' && strtoupper($row["transac_type"]) == strtoupper("delivery")) { ?>
+        <a title="Cancel" type="button" class="btn btn-xs btn-danger "
+          onclick="return confirm('Apakah Anda ingin membatalkan transaksi?')"
+          href="confirm.php?action=edit & cancel=<?php echo $row['transac_code']; ?> "><i
+            class="fas fa-minus-circle"></i>Batal</a>
+        <a title="Confirm" type="button" class="btn btn-xs btn-info "
+          onclick="return confirm('Apakah Anda ingin mengkonfirmasi transaksi?')"
+          href="confirm.php?action=edit & confirm=<?php echo $row['transac_code']; ?>"><i
+            class="fas fa-check"></i>Konfirmasi</a>
+        <a href="detail.php" class="btn btn-xs btn-warning"><i class="fas fa-sign-out-alt"></i>Kembali</a>
 
-          <?php } elseif (strtoupper($row["status"]) == '1' && strtoupper($row["transac_type"]) == strtoupper("delivery")) { ?>
-            <!-- <a title="Cancel" type="button" class="btn btn-xs btn-danger "
+        <?php } elseif (strtoupper($row["status"]) == '1' && strtoupper($row["transac_type"]) == strtoupper("delivery")) { ?>
+        <!-- <a title="Cancel" type="button" class="btn btn-xs btn-danger "
           onclick="return confirm('Apakah Anda ingin membatalkan transaksi?')"
           href="confirm.php?action=edit & cancel=<?php echo $row['transac_code']; ?> "><i
             class="fas fa-minus-circle"></i>Batal</a> -->
-            <a href="detail.php" class="btn btn-xs btn-warning"><i class="fas fa-sign-out-alt"></i>Kembali</a>
-            <!-- <?php echo '<a href="delivery_letter.php?id=' . $cd . '" class="btn btn-xs btn-info"><i class="fas fa-sign-out-alt"></i>Surat Jalan</a>' ?>
+        <a href="detail.php" class="btn btn-xs btn-warning"><i class="fas fa-sign-out-alt"></i>Kembali</a>
+        <!-- <?php echo '<a href="delivery_letter.php?id=' . $cd . '" class="btn btn-xs btn-info"><i class="fas fa-sign-out-alt"></i>Surat Jalan</a>' ?>
          <a title="confirmdelivery" type="button" class="btn btn-xs btn-success"
           onclick="confirmf(<?php echo $row['transac_code']; ?>)"><i class="fas fa-minus-circle"></i>Konfirmasi telah
           dikirim</a> -->
 
-          <?php } elseif (strtoupper($row["status"]) == '2' && strtoupper($row["transac_type"]) == strtoupper("delivery")) { ?>
-            <!-- <a title="Confirm" type="button" class="btn btn-xs btn-info "
+        <?php } elseif (strtoupper($row["status"]) == '2' && strtoupper($row["transac_type"]) == strtoupper("delivery")) { ?>
+        <!-- <a title="Confirm" type="button" class="btn btn-xs btn-info "
           onclick="return confirm('Apakah Anda ingin mengkonfirmasi transaksi?')"><i
             class="fas fa-check"></i>Konfirmasi</a> -->
-            <a href="detail.php" class="btn btn-xs btn-warning"><i class="fas fa-sign-out-alt"></i>Kembali</a>
-          <?php } elseif (strtoupper($row["status"]) == 'dp' && strtoupper($row["transac_type"]) == strtoupper("catering")) { ?>
-            <a href="controller/admin_delivery_controller.php?no_transac=<?php echo $cd; ?>&action=confirm" class="btn btn-xs btn-info"><i class="fas fa-sign-out-alt"></i>Buat Pesanan</a>
-            <a href="detail.php" class="btn btn-xs btn-warning"><i class="fas fa-sign-out-alt"></i>Kembali</a>
-          <?php } elseif (strtoupper($row["status"]) == 'confirmed' && strtoupper($row["transac_type"]) == strtoupper("delivery")) { ?>
-            <a href="controller/admin_delivery_controller.php?no_transac=<?php echo $cd; ?>&action=send" class="btn btn-xs btn-danger"><i class="fas fa-sign-out-alt"></i>Kirimkan Pesanan</a>
-            <a href="detail.php" class="btn btn-xs btn-warning"><i class="fas fa-sign-out-alt"></i>Kembali</a>
-          <?php }
+        <a href="detail.php" class="btn btn-xs btn-warning"><i class="fas fa-sign-out-alt"></i>Kembali</a>
+        <?php } elseif (strtoupper($row["status"]) == 'dp' && strtoupper($row["transac_type"]) == strtoupper("catering")) { ?>
+        <a href="controller/admin_delivery_controller.php?no_transac=<?php echo $cd; ?>&action=confirm"
+          class="btn btn-xs btn-info"><i class="fas fa-sign-out-alt"></i>Buat Pesanan</a>
+        <a href="detail.php" class="btn btn-xs btn-warning"><i class="fas fa-sign-out-alt"></i>Kembali</a>
+        <?php } elseif (strtoupper($row["status"]) == 'confirmed' && strtoupper($row["transac_type"]) == strtoupper("delivery")) { ?>
+        <a href="controller/admin_delivery_controller.php?no_transac=<?php echo $cd; ?>&action=send"
+          class="btn btn-xs btn-danger"><i class="fas fa-sign-out-alt"></i>Kirimkan Pesanan</a>
+        <a href="detail.php" class="btn btn-xs btn-warning"><i class="fas fa-sign-out-alt"></i>Kembali</a>
+        <?php }
 
 
 
@@ -228,25 +241,32 @@ if (!isset($_SESSION["userid"])) {
 
                 elseif (strtoupper($row["status"]) == strtoupper("pending") && strtoupper($row["transac_type"]) == "catering") { ?>
 
-            <a href="detail.php" class="btn btn-xs btn-warning"><i class="fas fa-sign-out-alt"></i>Kembali</a>
-          <?php } elseif (strtoupper($row["status"]) == strtoupper("dp") || strtoupper($row["status"]) == strtoupper("after_revision") && strtoupper($row["transac_type"]) == strtoupper("catering")) { ?>
-            <a href="controller/admin_catering_controller.php?no_transac=<?php echo $cd; ?>&action=confirm" class="btn btn-xs btn-info"><i class="fas fa-sign-out-alt"></i>Terima Pesanan</a>
-            <a href="controller/admin_catering_controller.php?no_transac=<?php echo $cd; ?>&action=deny" class="btn btn-xs btn-danger"><i class="fas fa-sign-out-alt"></i>Tolak Pesanan</a>
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#update_modal<?php echo $_GET['id']; ?>">Transfer Tidak Sesuai</button>
-            <a href="detail.php" class="btn btn-xs btn-warning"><i class="fas fa-sign-out-alt"></i>Kembali</a>
-          <?php } elseif (strtoupper($row["status"]) == strtoupper("pelunasan") || strtoupper($row["status"]) == strtoupper("after_revision_lns") && strtoupper($row["transac_type"]) == strtoupper("catering")) { ?>
-            <a href="controller/admin_catering_controller.php?no_transac=<?php echo $cd; ?>&action=lunas" class="btn btn-xs btn-info"><i class="fas fa-sign-out-alt"></i>Buat Pesanan</a>
-            <a href="controller/admin_catering_controller.php?no_transac=<?php echo $cd; ?>&action=deny" class="btn btn-xs btn-danger"><i class="fas fa-sign-out-alt"></i>Batalkan Pesanan</a>
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#update_modal<?php echo $_GET['id']; ?>">Transfer Tidak Sesuai</button>
-            <a href="detail.php" class="btn btn-xs btn-warning"><i class="fas fa-sign-out-alt"></i>Kembali</a>
-          <?php } elseif (strtoupper($row["status"]) == strtoupper('confirmed') && strtoupper($row["transac_type"]) == strtoupper("catering")) { ?>
-            <span>Menunggu Customer Melakukan Pelunasan</span>
-          <?php } elseif (strtoupper($row["status"]) == strtoupper('lunas') && strtoupper($row["transac_type"]) == strtoupper("catering")) { ?>
-            <span>Pesanan harus dikirimkan sesuai dengan tanggal yang tertera</span><br>
-          <?php } elseif (strtoupper($row["status"]) == 'confirmed' && strtoupper($row["transac_type"]) == "catering") { ?>
-            <a href="controller/admin_delivery_controller.php?no_transac=<?php echo $cd; ?>&action=send" class="btn btn-xs btn-danger"><i class="fas fa-sign-out-alt"></i>Kirimkan Pesanan</a>
-            <a href="detail.php" class="btn btn-xs btn-warning"><i class="fas fa-sign-out-alt"></i>Kembali</a>
-          <?php } ?>
+        <a href="detail.php" class="btn btn-xs btn-warning"><i class="fas fa-sign-out-alt"></i>Kembali</a>
+        <?php } elseif (strtoupper($row["status"]) == strtoupper("dp") || strtoupper($row["status"]) == strtoupper("after_revision") && strtoupper($row["transac_type"]) == strtoupper("catering")) { ?>
+        <a href="controller/admin_catering_controller.php?no_transac=<?php echo $cd; ?>&action=confirm"
+          class="btn btn-xs btn-info"><i class="fas fa-sign-out-alt"></i>Terima Pesanan</a>
+        <a href="controller/admin_catering_controller.php?no_transac=<?php echo $cd; ?>&action=deny"
+          class="btn btn-xs btn-danger"><i class="fas fa-sign-out-alt"></i>Tolak Pesanan</a>
+        <button type="button" class="btn btn-primary" data-toggle="modal"
+          data-target="#update_modal<?php echo $_GET['id']; ?>">Transfer Tidak Sesuai</button>
+        <a href="detail.php" class="btn btn-xs btn-warning"><i class="fas fa-sign-out-alt"></i>Kembali</a>
+        <?php } elseif (strtoupper($row["status"]) == strtoupper("pelunasan") || strtoupper($row["status"]) == strtoupper("after_revision_lns") && strtoupper($row["transac_type"]) == strtoupper("catering")) { ?>
+        <a href="controller/admin_catering_controller.php?no_transac=<?php echo $cd; ?>&action=lunas"
+          class="btn btn-xs btn-info"><i class="fas fa-sign-out-alt"></i>Buat Pesanan</a>
+        <a href="controller/admin_catering_controller.php?no_transac=<?php echo $cd; ?>&action=deny"
+          class="btn btn-xs btn-danger"><i class="fas fa-sign-out-alt"></i>Batalkan Pesanan</a>
+        <button type="button" class="btn btn-primary" data-toggle="modal"
+          data-target="#update_modal<?php echo $_GET['id']; ?>">Transfer Tidak Sesuai</button>
+        <a href="detail.php" class="btn btn-xs btn-warning"><i class="fas fa-sign-out-alt"></i>Kembali</a>
+        <?php } elseif (strtoupper($row["status"]) == strtoupper('confirmed') && strtoupper($row["transac_type"]) == strtoupper("catering")) { ?>
+        <span>Menunggu Customer Melakukan Pelunasan</span>
+        <?php } elseif (strtoupper($row["status"]) == strtoupper('lunas') && strtoupper($row["transac_type"]) == strtoupper("catering")) { ?>
+        <span>Pesanan harus dikirimkan sesuai dengan tanggal yang tertera</span><br>
+        <?php } elseif (strtoupper($row["status"]) == 'confirmed' && strtoupper($row["transac_type"]) == "catering") { ?>
+        <a href="controller/admin_delivery_controller.php?no_transac=<?php echo $cd; ?>&action=send"
+          class="btn btn-xs btn-danger"><i class="fas fa-sign-out-alt"></i>Kirimkan Pesanan</a>
+        <a href="detail.php" class="btn btn-xs btn-warning"><i class="fas fa-sign-out-alt"></i>Kembali</a>
+        <?php } ?>
 
         <?php    }  ?>
 
@@ -271,9 +291,9 @@ if (!isset($_SESSION["userid"])) {
 
 
 
-        </div>
       </div>
-      <?php
+    </div>
+    <?php
       $query = 'SELECT * FROM tbltransacdetail
               WHERE
               transac_code ="' . $_GET['id'] . '"';
@@ -284,17 +304,17 @@ if (!isset($_SESSION["userid"])) {
 
 
       ?>
-      <?php
+    <?php
 
 
 
       ?>
 
-    </div>
+  </div>
 
 
 
-  </div><br>
+</div><br>
 
 
 <?php include 'theme/footer.php';
