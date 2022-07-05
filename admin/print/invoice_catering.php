10c0147fc1e04bbf38b60207c72645579bd96890
Sandria Maulana <sandriamaulana31@gmail.com>
2022-07-05 22:33:09 +0700

    Fix alamat invoice

--- admin/print/invoice_catering.php
+++ admin/print/invoice_catering.php
@@ -112,7 +112,7 @@ if ($invoice['status'] == "dp") {
 $pdf->SetFont('Arial', 'B', 10);
 $pdf->Cell(34, 5, "Pembayaran " . $status . " 50%", 0, 1); //end of line
 $pdf->SetFont('Arial', '', 10);
-$pdf->Cell(100, 15, 'Alamat : ' . $invoice['alamat'], 0, 0);
+$pdf->Cell(100, 15, '', 0, 0);
 $pdf->Cell(25, 5, '', 0, 0);
 $pdf->SetFont('Arial', 'B', 10);
 $pdf->Cell(34, 5, 'atas Pemesanan Catering Tgl ' . $invoice['date'], 0, 1); //end of line
@@ -121,7 +121,10 @@ $pdf->SetFont('Arial', '', 10);
 $pdf->Cell(189, 10, '', 0, 1); //end of line
 
 //billing address
-$pdf->Cell(100, 5, 'Detail Pesanan', 0, 1); //end of line
+$pdf->SetFont('Arial', 'B', 10);
+$pdf->MultiCell(100, 5, "Alamat Pengiriman", 0, 1);
+$pdf->SetFont('Arial', '', 10);
+$pdf->MultiCell(100, 5, $invoice['alamat'], 0, 1); //end of line
 
 $pdf->LN();
 
