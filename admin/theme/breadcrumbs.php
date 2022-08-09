382f59fa61fcf6ed01e96b2b1754fb2a45c55d8b
Muhammad Ridwan Eryansah <mridwane01@gmail.com>
2022-08-09 11:54:17 +0700

    benerin tampilan home dashboard admin

--- admin/theme/breadcrumbs.php
+++ admin/theme/breadcrumbs.php
@@ -8,7 +8,7 @@ $row = mysqli_fetch_assoc($result);
 ?>
 
 <!-- Icon Cards-->
-<div class="row">
+<!-- <div class="row">
   <div class="col-xl-3 col-sm-6 mb-3">
     <div class="card text-white bg-primary o-hidden h-100">
       <div class="card-body">
@@ -80,8 +80,23 @@ $row = mysqli_fetch_assoc($result);
       </a>
     </div>
   </div>
-</div>
+</div> -->
 
+<div class="row">
+  <div class="col-6">
+    <div class="jumbotron">
+      <h1 class="display-4">Selamat datang <?= $_SESSION['fname'].' '.$_SESSION['lname'] ?></h1>
+      <!-- <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to
+        featured content or information.</p> -->
+      <hr class="my-4">
+      <!-- <p>It uses utility classes for typography and spacing to space content out within the larger container.</p> -->
+      <!-- <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a> -->
+    </div>
+  </div>
+  <div class="col-6">
+    <img src="images/welcome.svg" alt="" width="450px">
+  </div>
+</div>
 
 
 
