15dbe8b09c5ae6ec67f4fd683580ba598aef40ec
Muhammad Ridwan Eryansah <mridwane01@gmail.com>
2022-08-13 10:54:22 +0700

    nambahin alasan ditolak

--- admin/login.php
+++ admin/login.php
@@ -58,7 +58,7 @@ else{
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Login - SB Admin</title>
+        <title>Login - Andakar admin</title>
         <!-- Custom fonts for this template-->
         <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
         <link href="css/styles.css" rel="stylesheet" />
