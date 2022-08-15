71a790d002e7249b0786d3e493eaf4b3cc5718c2
Sandria Maulana <sandriamaulana31@gmail.com>
2022-08-15 22:03:34 +0700

    nambahin croon untuk cancelotomati  transaksi catering dan reservasi jika melewati tanggal yang diipiliih

---
+++ cron_status.php
@@ -0,0 +1,25 @@
+<!-- cron ini akan dijalankan di server -->
+<?php
+include('includes/connection.php');
+$query = 'SELECT * FROM tbltransac WHERE transac_type = "Reservasi" or transac_type = "Catering"';
+
+$result = mysqli_query($db, $query) or die(mysqli_error($db));
+$date = date("Y-m-d");
+while ($row = mysqli_fetch_array($result)) {
+    if ($row["status"] != "done" && $row["status"] != "lunas") {
+        if ($row["transac_type"] == "Reservasi") {
+            if (date("d", strtotime($row["reservation_date_time"])) - date("d", strtotime($date)) < 0) {
+                $query2 = "UPDATE tbltransac SET status = 'cancel' WHERE transac_code='" . $row['transac_code'] . "'";
+                mysqli_query($db, $query2) or die(mysqli_error($db));
+                echo "berhasil reser";
+            }
+        } else if ($row["transac_type"] == "Catering") {
+
+            if (date("d", strtotime($row["reservation_date_time"])) - date("d", strtotime($date)) < 2) {
+                $query3 = "UPDATE tbltransac SET status = 'cancel' WHERE transac_code='" . $row['transac_code'] . "'";
+                mysqli_query($db, $query3) or die(mysqli_error($db));
+                echo "berhasil cat";
+            }
+        }
+    }
+}
