a8b5743411b662c4a634798dd0e4b571b0b32a71
Sandria Maulana <sandriamaulana31@gmail.com>
2022-08-15 22:26:33 +0700

    update alert

--- cron_status.php
+++ cron_status.php
@@ -11,14 +11,14 @@ while ($row = mysqli_fetch_array($result)) {
             if (date("d", strtotime($row["reservation_date_time"])) - date("d", strtotime($date)) < 0) {
                 $query2 = "UPDATE tbltransac SET status = 'cancel' WHERE transac_code='" . $row['transac_code'] . "'";
                 mysqli_query($db, $query2) or die(mysqli_error($db));
-                echo "berhasil reser";
+                echo $row['transac_code'] . " berhasil di batalkan" . "<br>";
             }
         } else if ($row["transac_type"] == "Catering") {
 
             if (date("d", strtotime($row["reservation_date_time"])) - date("d", strtotime($date)) < 2) {
                 $query3 = "UPDATE tbltransac SET status = 'cancel' WHERE transac_code='" . $row['transac_code'] . "'";
                 mysqli_query($db, $query3) or die(mysqli_error($db));
-                echo "berhasil cat";
+                echo $row['transac_code'] . " berhasil di batalkan" . "<br>";
             }
         }
     }
